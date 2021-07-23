cd376cf9c70b3228ad7a3371d88abf5574a6b741
Les Carbonaro <lcarbonaro@gmail.com>
2021-07-23 12:25:52 -0400

    Corrected import

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '/src/components/Home.vue';
 import About from '/src/components/About.vue';
 
